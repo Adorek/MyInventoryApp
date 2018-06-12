d6873b2beb9689806072535bb97ec042c40d89ed
Adorek <adam.rafinski@gmail.com>
2018-06-12 18:38:33 +0200

    Initial commit, with some TODO's and before clean up.

---
+++ app/src/main/java/com/example/android/myinventoryapp/data/ProductContract.java
@@ -0,0 +1,20 @@
+package com.example.android.myinventoryapp.data;
+
+import android.provider.BaseColumns;
+
+public final class ProductContract {
+
+    private ProductContract() {}
+
+    public static abstract class ProductEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "products";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+        public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public static final String COLUMN_PRODUCT_SUPPLIER = "supplier";
+        public static final String COLUMN_PRODUCT_PHONE = "phone";
+    }
+}
---
+++ app/src/main/java/com/example/android/myinventoryapp/data/ProductDbHelper.java
@@ -0,0 +1,47 @@
+package com.example.android.myinventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.myinventoryapp.data.ProductContract.ProductEntry;
+
+public class ProductDbHelper extends SQLiteOpenHelper {
+
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "store.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link ProductDbHelper}.
+     *
+     * @param context of the app
+     */
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Create a String that contains the SQL statement to create the products table
+        String SQL_CREATE_PRODUCTS_TABLE =  "CREATE TABLE " + ProductEntry.TABLE_NAME + " ("
+                + ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_SUPPLIER + " TEXT, "
+                + ProductEntry.COLUMN_PRODUCT_PHONE + " INTEGER NOT NULL DEFAULT 555666777);";
+
+        // Execute the SQL statement
+        sqLiteDatabase.execSQL(SQL_CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
